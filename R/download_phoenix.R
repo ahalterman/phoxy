b9b94da0c02bc4a144a9ad17e5a73161206bf0b0
Andy Halterman <ahalterman0@gmail.com>
2015-01-10 10:28:14 -0500

    Initial commit

---
+++ R/download_phoenix.R
@@ -0,0 +1,8 @@
+#' Downloads a complete copy of the Phoenix dataset into a given directory
+#'
+#' 
+
+#' #' @export
+download_phoenix <- function(){
+  
+}
---
+++ R/update_phoenix.R
@@ -0,0 +1,11 @@
+#' Update a local directory of Phoenix dataset files with new files from the server
+#'
+#' Checks the contents of a directory containing Phoenix event data files, checks whether the 
+#' server has new events, and downloads them to that directory. (It'll have some version handling ability,
+#' too, either from the file names or by reading in the events.)
+#'
+
+#' #' @export
+update_phoenix <- function(){
+  
+}
