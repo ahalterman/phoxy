cfd9938b40faa54f3105dce4c8f064c2f29eec2e
Andy Halterman <ahalterman0@gmail.com>
2015-01-11 13:24:18 -0500

    Basic working functionality

--- R/download_phoenix.R
+++ R/download_phoenix.R
@@ -1,8 +1,60 @@
-#' Downloads a complete copy of the Phoenix dataset into a given directory
+#' Download the Phoenix Dataset
 #'
-#' 
+#' Download and unzip all of the data files for the Phoenix dataset from the 
+#' Phoenix data website into a given directory.
+#'
+#' @param dir The path to download Phoenix into.
+#' @param version [Not yet implemented]. Download a specific version of Phoenix.
+#'
+#' @return NULL
+#' @author Andy Halterman
+#' @note This function, like Phoenix, is still in development and may contain errors and change quickly.
+#' @examples
+#'
+#' download_phoenix("~/OEDA/phoxy_test/")
+#'
+#' @rdname download_phoenix
 
-#' #' @export
-download_phoenix <- function(){
+get_links <- function(){
+  library(rvest)
+  data_page <- rvest::html("http://phoenixdata.org/data")
   
+  page <- data_page %>%
+    html_node("tbody") %>%
+    html_text
+  
+  page <- unlist(stringr::str_split(page, "\n"))
+  page <- stringr::str_trim(page)
+  
+  links <- page[grep("https://s3.amazonaws.com/oeda/data/events", page)]
+  return(links)
 }
+
+
+
+#' @export
+#' @importFrom plyr l_ply progress_text
+
+download_phoenix <- function(destpath){
+  # Get all the links on the page
+  library(RCurl)
+  ll <- get_links()
+  
+  # given a list of links, download them and write to specified directory
+  dw_file <- function(link){
+    filename = gsub("https://s3.amazonaws.com/oeda/data/", "", link)
+    bin <- getBinaryURL(link, ssl.verifypeer=FALSE)
+    con <- file(filename, open = "wb")
+    writeBin(bin, con)
+    close(con)
+    unzip(filename, exdir = ".", unzip = "internal", setTimes = FALSE)
+    unlink(filename)
+}
+  message("Downloading and unzipping files.")
+  plyr::l_ply(ll, dw_file, .progress = plyr::progress_text(char = '*'))
+}
+
+
+
+
+
--- R/ingest_phoenix.R
+++ R/ingest_phoenix.R
@@ -1,4 +1,20 @@
-
+#' Ingest the Phoenix Dataset
+#'
+#' Given a directory with individual Phoenix dataset files, quickly read 
+#' them all in, name them correctly, and combine them into one dataframe.
+#'
+#' @param dir The path to the Phoenix folder.
+#' @param version [Not yet implemented]. Use the appropriate function for each Phoenix version.
+#' @param read_func [Not yet implemented]. Use an alternative reading function like \code{fread} or \code{read_csv}.
+#'
+#' @return A single dataframe with all the Phoenix events in the folder.
+#' @author Andy Halterman
+#' @note This function, like Phoenix, is still in development and may contain errors and change quickly.
+#' @examples
+#'
+#' events <- ingest_phoenix("~/OEDA/phoxy_test/")
+#' 
+#' @rdname ingest_phoenix
 #' @export
 ingest_phoenix <- function(dir, version = "auto", read_func = "read.csv"){  
   # Handle messy file paths
@@ -11,8 +27,21 @@ ingest_phoenix <- function(dir, version = "auto", read_func = "read.csv"){
   files <- paste0(dir, files)
   eventColClasses <- c(rep("integer", 5), rep("character", 8), rep("factor", 3), 
                        "numeric", "character", "numeric", "numeric", rep("character", 6))
-  event_list  <- lapply(files, read.csv, stringsAsFactors=FALSE, header=FALSE, colClasses=eventColClasses, sep="\t")
+  
+  # A reading function with some error catching.
+  read_one <- function(file){
+    t <- tryCatch(read.csv(file, stringsAsFactors=FALSE, header=FALSE, 
+                      colClasses=eventColClasses, sep="\t", quote = ""), error=function(e) NULL)
+    if(class(t)[1] == "data.frame"){
+      return(t)
+    }
+  }
+  
+  message("Reading in files...")
+  event_list  <- plyr::llply(files, read_one, .progress = plyr::progress_text(char = '*'))
+  #event_list  <- lapply(files, read_one)
   events <- dplyr::rbind_all(event_list)
+  #print(str(events))
   names(events) <- c("EventID", "Date", "Year", "Month", "Day", "SourceActorFull", 
                      "SourceActorEntity", "SourceActorRole", "SourceActorAttribute", 
                      "TargetActorFull", "TargetActorEntity", "TargetActorRole", 
@@ -22,4 +51,7 @@ ingest_phoenix <- function(dir, version = "auto", read_func = "read.csv"){
                      "NewsSources")
   return(events)
 }
+
+
+
   
