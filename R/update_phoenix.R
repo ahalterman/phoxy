19d099d1c3ca4a1dd8dc89155d6c1b3d391673e0
Andy Halterman <ahalterman0@gmail.com>
2015-02-18 20:25:32 -0500

    Convert date to Date class; scaffold for streaming filter

--- R/update_phoenix.R
+++ R/update_phoenix.R
@@ -5,7 +5,36 @@
 #' too, either from the file names or by reading in the events.)
 #'
 
+
 #' #' @export
-update_phoenix <- function(){
+update_phoenix <- function(destpath){
+  library(RCurl)
+  links <- phoxy:::get_links()  # pulls all the links from the OEDA Phoenix page
+  links_shortened <- as.data.frame(stringr::str_match(links, "events.full.(\\d+).txt"), stringsAsFactors=FALSE)
+  filelist <- list.files(destpath)
+  filelist_shortened <- as.data.frame(stringr::str_match(filelist, "events.full.(\\d+).txt"), stringsAsFactors=FALSE)
+  #print(head(filelist_shortened))
+  #print(head(links_shortened))
+  # All rows in links_shortened that do not have a match in filelist_shortened.
+  new_files <- dplyr::anti_join(links_shortened, filelist_shortened, by = "V2")
+  message("There are ", nrow(new_files), " undownloaded daily files. Downloading now...")
+
+  ll <- paste0("https://s3.amazonaws.com/oeda/data/", new_files$V1, ".zip")
   
+  # given a list of links, download them and write to specified directory
+  dw_file <- function(link){
+    filename <- gsub("https://s3.amazonaws.com/oeda/data/", "", link)
+    filename <- paste0(destpath, filename)
+    bin <- getBinaryURL(link, ssl.verifypeer=FALSE)
+    con <- file(filename, open = "wb")
+    writeBin(bin, con)
+    close(con)
+    unzip(filename, exdir = destpath, unzip = "internal", setTimes = FALSE)
+    unlink(filename)
+  }
+  message("Downloading and unzipping files.")
+  plyr::l_ply(ll, dw_file, .progress = plyr::progress_text(char = '='))
 }
+
+
+
