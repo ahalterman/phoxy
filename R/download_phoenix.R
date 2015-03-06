56eba6311f5db2570fb2628cb26a39410219a1df
Andy Halterman <ahalterman0@gmail.com>
2015-03-06 09:58:54 -0500

    Make it work with the new URL and versioning setup

--- R/download_phoenix.R
+++ R/download_phoenix.R
@@ -15,44 +15,47 @@
 #'
 #' @rdname download_phoenix
 
-get_links <- function(){
+# get all the URLs on a page
+get_links <- function(phoenix_version){
   library(rvest) # I know...not best practices...
-  data_page <- rvest::html("http://phoenixdata.org/data")
+  page_url <- paste0("http://phoenixdata.org/data/", phoenix_version)
+  data_page <- rvest::html(page_url)
   
   page <- data_page %>%
     html_node("tbody") %>%
     html_text
   
-  page <- unlist(stringr::str_split(page, "\n"))
+  page <- unlist(stringr::str_split(page, " "))
   page <- stringr::str_trim(page)
   
-  links <- page[grep("https://s3.amazonaws.com/oeda/data/events", page)]
+  links <- gsub("\\d{4}\\-\\d{2}\\-\\d{2}$", "", page)
+  links <- links[links != ""]
   return(links)
 }
 
-
+# given a list of links, download them and write to specified directory
+dw_file <- function(link, destpath, phoenix_version){
+  baseurl <- paste0("https://s3.amazonaws.com/oeda/data/", phoenix_version, "/")
+  filename <- gsub(baseurl, "", link)
+  filename <- paste0(destpath, filename)
+  bin <- getBinaryURL(link, ssl.verifypeer=FALSE)
+  con <- file(filename, open = "wb")
+  writeBin(bin, con)
+  close(con)
+  unzip(filename, exdir = destpath, unzip = "internal", setTimes = FALSE)
+  unlink(filename)
+}
 
 #' @export
 #' @importFrom plyr l_ply progress_text
-
-download_phoenix <- function(destpath){
-  # Get all the links on the page
+download_phoenix <- function(destpath, phoenix_version = "current"){
   library(RCurl)
-  ll <- get_links()
-  
-  # given a list of links, download them and write to specified directory
-  dw_file <- function(link){
-    filename <- gsub("https://s3.amazonaws.com/oeda/data/", "", link)
-    filename <- paste0(destpath, filename)
-    bin <- getBinaryURL(link, ssl.verifypeer=FALSE)
-    con <- file(filename, open = "wb")
-    writeBin(bin, con)
-    close(con)
-    unzip(filename, exdir = destpath, unzip = "internal", setTimes = FALSE)
-    unlink(filename)
-}
+  if (stringr::str_sub(destpath, -1) != "/"){
+    stop("Destination paths need to have trailing forward slashes")
+  }
+  ll <- get_links(phoenix_version = phoenix_version)
   message("Downloading and unzipping files.")
-  plyr::l_ply(ll, dw_file, .progress = plyr::progress_text(char = '='))
+  plyr::l_ply(ll, dw_file, phoenix_version = phoenix_version, destpath = destpath, .progress = plyr::progress_text(char = '='))
 }
 
 
