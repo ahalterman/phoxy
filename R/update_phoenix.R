b71169890508e46a947a5e5bf84e3bdbebc0502f
Andy Halterman <ahalterman0@gmail.com>
2015-03-10 14:06:44 -0400

    Works with different Phoenix versions now

--- R/update_phoenix.R
+++ R/update_phoenix.R
@@ -4,37 +4,37 @@
 #' server has new events, and downloads them to that directory. (It'll have some version handling ability,
 #' too, either from the file names or by reading in the events.)
 #'
+#' @param destpath The path to download Phoenix into.
+#' @param phoenix_version. Download a specific version of Phoenix ("v0.1.0" or "current").
+#'
+#' @return NULL
+#' @author Andy Halterman
+#' @note This function, like Phoenix, is still in development and may contain errors and change quickly.
+#' @examples
+#'
+#' update_phoenix("~/OEDA/phoxy_test/", phoenix_version = "current")
 
-
-#' #' @export
-update_phoenix <- function(destpath){
+#' 
+#' @export
+update_phoenix <- function(destpath, phoenix_version = "current"){
   library(RCurl)
-  links <- phoxy:::get_links()  # pulls all the links from the OEDA Phoenix page
+  # pulls all the links from the OEDA Phoenix page
+  links <- phoxy:::get_links(phoenix_version = phoenix_version)  
   links_shortened <- as.data.frame(stringr::str_match(links, "events.full.(\\d+).txt"), stringsAsFactors=FALSE)
   filelist <- list.files(destpath)
   filelist_shortened <- as.data.frame(stringr::str_match(filelist, "events.full.(\\d+).txt"), stringsAsFactors=FALSE)
-  #print(head(filelist_shortened))
-  #print(head(links_shortened))
   # All rows in links_shortened that do not have a match in filelist_shortened.
   new_files <- dplyr::anti_join(links_shortened, filelist_shortened, by = "V2")
   message("There are ", nrow(new_files), " undownloaded daily files. Downloading now...")
-
-  ll <- paste0("https://s3.amazonaws.com/oeda/data/", new_files$V1, ".zip")
   
-  # given a list of links, download them and write to specified directory
-  dw_file <- function(link){
-    filename <- gsub("https://s3.amazonaws.com/oeda/data/", "", link)
-    filename <- paste0(destpath, filename)
-    bin <- getBinaryURL(link, ssl.verifypeer=FALSE)
-    con <- file(filename, open = "wb")
-    writeBin(bin, con)
-    close(con)
-    unzip(filename, exdir = destpath, unzip = "internal", setTimes = FALSE)
-    unlink(filename)
-  }
+  version_nodots <- gsub(".", "", phoenix_version, fixed=TRUE)
+  ll <- paste0("https://s3.amazonaws.com/oeda/data/", version_nodots, "/", new_files$V1, ".zip")
+  
   message("Downloading and unzipping files.")
-  plyr::l_ply(ll, dw_file, .progress = plyr::progress_text(char = '='))
+  plyr::l_ply(ll, phoxy:::dw_file, destpath = destpath, phoenix_version = phoenix_version, .progress = plyr::progress_text(char = '='))
 }
+
+
 
 
 
